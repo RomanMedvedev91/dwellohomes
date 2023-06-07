42439cf1a745c0bcb24b96d41ab47dcaf22f2e1e
Roman Medvedev <roman.medvedev77@gmail.com>
2023-06-07 08:27:07 -0400

    add post and delete routes, bodyparser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,25 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 9000;
+import { listings } from './listings';
 
+app.use(bodyParser.json());
+// app.use(express.json()); // alternatively express provide parser
 app.get('/', (_req, res) => res.send('hello world'));
+
+app.get('/listings', (_req, res) => {
+  res.send(listings);
+});
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+  for (let i = 0; i < listings.length; i++) {
+    if(listings[i].id === id) {
+     return res.send(listings.splice(i, 1));
+    }
+  }
+  return res.send('failed to delete listing');
+});
 app.listen(port);
 
 console.log(`[app] : http://localhost:${port}`);
