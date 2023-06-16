2bf5b54468181f6cc759b7cef0303f8bcc87f994
roman <roman.medvedev77@gmail.com>
2023-06-15 23:57:28 -0400

    add temp seeds

---
+++ server/temp/seed.ts
@@ -0,0 +1,13 @@
+require("dotenv").config();
+import { connectDatabase } from "../src/database";
+
+const seed = async () => {
+  try {
+    console.log(`[seed] : running...`);
+    const db = await connectDatabase();
+  } catch (error) {
+    throw new Error("failed to seed database");
+  }
+};
+
+seed();
