b3c1e722ef5c819645c1de2a105008edc997998a
roman <roman.medvedev77@gmail.com>
2023-06-16 23:51:38 -0400

    change listings in resolver

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,10 +1,11 @@
 import { IResolvers } from 'apollo-server-express';
+import { Database } from "../lib/types";
 import { listings } from '../listings';
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (_root: undefined, _args: {}, { db }: { db: Database }) => {
+      return await db.listings.find({}).toArray();
     },
   },
 
