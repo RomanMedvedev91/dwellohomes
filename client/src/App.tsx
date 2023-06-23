0f6e0722659b1f5817decb129bc55cfaa06b1599
roman <roman.medvedev77@gmail.com>
2023-06-22 23:52:09 -0400

    add listings component structure

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Listings } from './sections';
+
+function App() {
+  return (
+    <Listings />
+  );
+}
+
+export default App;
