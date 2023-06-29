0e5440b51671eb8b9abe655b8e1a06cd021bed1e
roman <roman.medvedev77@gmail.com>
2023-06-28 22:33:22 -0400

    add useQuery and useMutation hook

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { server } from '../../lib/api';
 import { DeleteListingData, DeleteListingVariables, ListingsData, type Listing } from "./types";
-import { useQuery } from '../../lib/api';
+import { useMutation, useQuery } from '../../lib/api';
 
 const LISTINGS = `
   query Listings {
@@ -32,15 +32,27 @@ interface IListingsProps {
 }
 
 export const  Listings = ({ title }: IListingsProps) => {
-  const { data, refetch } = useQuery<ListingsData>(LISTINGS);
+  const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
+  const [deleteListing, {
+    loading: deleteListingLoading,
+    error: deleteListingError
+  }] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTING);
 
-  const deleteListing = async (id: string) => {
-    await server.fetch<DeleteListingData, DeleteListingVariables>({
-      query: DELETE_LISTING,
-      variables: {
-        id
-      }
-    });
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+  if(error) {
+    return <h2>Oh! something went wrong - please try again</h2>
+  }
+  const deleteListingLoadingMessage = deleteListingLoading ? (
+    <h4>Deletion in progress...</h4>
+  ) : null;
+  const deleteListingErrorMessage = deleteListingError ? (
+    <h4>Uh oh! Something went wrong with deleting :(. Please try again soon.</h4>
+  ) : null;
+
+  const handleDeleteListing = async (id: string) => {
+    await deleteListing({ id })
     refetch();
   };
 
@@ -51,12 +63,14 @@ export const  Listings = ({ title }: IListingsProps) => {
           <li key={listing.id}>
             {listing.title}{" "}
             <button 
-              onClick={() => deleteListing(listing.id)}
+              onClick={() => handleDeleteListing(listing.id)}
             >
               Delete
             </button>
             </li>
         ))}
+        {deleteListingLoadingMessage}
+        {deleteListingErrorMessage}
       <ul>
       </ul>
     </div>
