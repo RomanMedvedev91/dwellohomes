e456919263265a8d020fe02e8b0807d573578cc3
roman <roman.medvedev77@gmail.com>
2023-07-01 14:32:18 -0400

    add ant design ui frameworks, add styles, change components

---
+++ client/src/sections/Listings/components/ListingsSkeleton/ListingsSkeleton.tsx
@@ -0,0 +1,28 @@
+import { Skeleton, Divider, Alert } from 'antd';
+
+import "./styles/ListingsSkeleton.css";
+import { ApolloError } from '@apollo/client';
+
+interface IListingsSkeletonProps {
+  title: string;
+  error?: ApolloError;
+}
+export default function ListingsSkeleton({ title, error }: IListingsSkeletonProps) {
+  return (
+    <div className="listings-skeleton">
+      {error ? (
+      <Alert
+        type="error"
+        message="Uh oh! Something went wrong :(. Please try again later."
+        className="listings-skeleton__alert"
+      />
+      ) : null}
+      <h2>{title}</h2>
+      <Skeleton active paragraph={{ rows: 1 }} />;
+      <Divider />
+      <Skeleton active paragraph={{ rows: 1 }} />;
+      <Divider />
+      <Skeleton active paragraph={{ rows: 1 }} />;
+    </div>
+  )
+}
