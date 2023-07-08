bcb6e5b98cfa6793eb50149eb95d45f820563976
roman <roman.medvedev77@gmail.com>
2023-07-08 10:53:37 -0400

    add login page ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { Layout } from "antd";
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+
+import './App.css';
 import { Home, Host, Listing, Listings, Login, NotFound, User } from './sections';
-import { Layout } from "antd";
+import { Viewer } from './lib/types';
+
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false
+};
 
 function App() {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
   return (
     <BrowserRouter>
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setViewer={setViewer} />} />
         <Route path="/host" element={<Host />} />
         <Route path="/listing/:id" element={<Listing />} />
         <Route path="/listings/:location?" element={<Listings title="Welcome to Dwello Homes" />} />
--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -1,15 +1,81 @@
-import React from 'react'
-import { Card, Layout, Typography } from "antd";
+import React, { useEffect, useRef  } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useApolloClient, useMutation } from '@apollo/client';
+import { Card, Layout, Typography, Spin } from "antd";
 
 // Image Assets
 import googleLogo from "./assets/google_logo.jpg";
+import { Viewer } from '../../lib/types';
+import { AUTH_URL } from "../../lib/graphql/queries";
+import { LOG_IN } from "../../lib/graphql/mutations";
+import { AuthUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl";
+import {
+  LogIn as LogInData,
+  LogInVariables
+} from "../../lib/graphql/mutations/LogIn/__generated__/LogIn";
+import { ErrorBanner } from "../../lib/components";
+import { displaySuccessNotification, displayErrorMessage } from "../../lib/utils";
 
 const { Content } = Layout;
 const { Text, Title } = Typography;
+interface ILoginProps {
+  setViewer: (viewer: Viewer) => void;
+}
 
-export const Login = () => {
+export const Login = ({ setViewer }: ILoginProps) => {
+  const client = useApolloClient();
+  let navigate = useNavigate();
+  const [
+    logIn,
+    { data: logInData, loading: logInLoading, error: logInError }
+  ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: data => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+        displaySuccessNotification("You've successfully logged in!");
+      }
+    }
+  });
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    if (code) {
+      logInRef.current({
+        variables: {
+          input: { code }
+        }
+      });
+    }
+  }, []);
+
+  const handleAuthorize = async () => {
+    try {
+      const { data } = await client.query<AuthUrlData>({
+        query: AUTH_URL
+      });
+      window.location.href = data.authUrl;
+    } catch(err) {
+      displayErrorMessage("Sorry! We weren't able to log you in. Please try again later!");
+    }
+  };
+
+  if (logInLoading) {
+    return (
+      <Content className="log-in">
+        <Spin size="large" tip="Logging you in..." />
+      </Content>
+    );
+  }
+
+  if (logInData && logInData.logIn) {
+    const { id: viewerId } = logInData.logIn;
+    navigate(`/user/${viewerId}`);
+  }
+
   return (
     <Content className="log-in">
+    {logInError && <ErrorBanner description="We weren't able to log you in. Please try again soon." />}
     <Card className="log-in-card">
       <div className="log-in-card__intro">
         <Title level={3} className="log-in-card__intro-title">
@@ -22,7 +88,7 @@ export const Login = () => {
         </Title>
         <Text>Sign in with Google to start booking available rentals!</Text>
       </div>
-      <button className="log-in-card__google-button">
+      <button className="log-in-card__google-button" onClick={handleAuthorize}>
           <img
             src={googleLogo}
             alt="Google Logo"
