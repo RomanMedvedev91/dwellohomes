c1d8c7b97fd82c76982323df5879c1e57e09fd99
roman <roman.medvedev77@gmail.com>
2023-07-13 09:15:26 -0400

    ad typeDefs for user type

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,7 +1,8 @@
 import merge from "lodash.merge";
 import { viewerResolvers } from "./Viewer";
+import { userResolvers } from "./User";
 
-export const resolvers = merge(viewerResolvers);
+export const resolvers = merge(viewerResolvers, userResolvers);
 
 // TODO: 
 // export const resolvers = merge(bookingResolvers, listingResolvers, userResolvers);
